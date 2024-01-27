cb6dbe67a4e3cf5bb46f2a151aa33feaa93d9ca9
Ahmad Searcy <ahmadsearcy7@gmail.com>
2024-01-27 02:22:47 -0600

    functionality complete
    from here I have completed most of the functionality changes associated with the todo website, however I still must include the date in which the entry was made alongside media queries

--- todo.js
+++ todo.js
@@ -1,28 +1,34 @@
-//declare dom variables
+// declare dom variables
 const title = document.getElementById("title-input");
 const description = document.getElementById("description-input");
 const entryContainer = document.getElementById("entry-container");
 const submitButton = document.getElementById("Submit");
 
-//add entry function. This function will take the string from the title and description and put it into an unordered list as div that has a h3 for the title and a p element for the description. These entries will also include and button that allows you to edit the entry. When you click on edit entry two buttons will appear, one that will complete the edit and another than deletes the entry. when you press the edit button it will turn the h2 into a text input and the p into a text area. once you hit complete edit, it will return the new values for the title and the description as an h3 and a p and revert back to the edit button
-
+// add entry function
 function addEntry() {
   if (title.value === "" || description.value === "") {
-    return alert("add text to entry and description box");
+    return alert("Add text to title and description boxes.");
   } else {
-    const entryCounter = document.querySelectorAll("#entry-container div") + 1;
-    const HTML = `<div id="entry-${entryCounter}"><h3 id="title-${entryCounter}">${title.value}</h3><p id="description-${entryCounter}">${description.value}</p><i onclick="editEntry('${entryCounter}')" id="edit-icon-${entryCounter}" class="fa-solid fa-pen-to-square"></i><br></div>`;
+    const entryCounter =
+      document.querySelectorAll("#entry-container div").length + 1;
+    const HTML = `
+      <div id="entry-${entryCounter}">
+        <h3 id="title-${entryCounter}">${title.value}</h3>
+        <p id="description-${entryCounter}">${description.value}</p>
+        <div id="button-container-${entryCounter}"><i onclick="editEntry(${entryCounter})" class="fa-solid fa-pen-to-square edit-icon"></i><div>
+        <br>
+      </div>`;
     entryContainer.insertAdjacentHTML("beforeend", HTML);
     title.value = "";
     description.value = "";
   }
 }
+
 submitButton.addEventListener("click", () => {
   addEntry();
 });
 
-//this is the function that deals with the edit button functionality
-
+// edit entry function
 function editEntry(entryCounter) {
   const titleInitialValue = document.getElementById(
     `title-${entryCounter}`
@@ -30,34 +36,40 @@ function editEntry(entryCounter) {
   const descriptionInitialValue = document.getElementById(
     `description-${entryCounter}`
   ).innerText;
-  const editButton = document.getElementById(`edit-icon-${entryCounter}`);
-  const editTitle = document.getElementById(`title-${entryCounter}`);
-  const editDescription = document.getElementById(
-    `description-${entryCounter}`
+  const buttonContainer = document.getElementById(
+    `button-container-${entryCounter}`
   );
-  editTitle.innerHTML = `<input placeholder="Title" id="title-input-${entryCounter}" type="text" /><br>`;
-  editDescription.innerHTML = `<textarea id="description-input-${entryCounter}" placeholder="description" cols="40" rows="10"></textarea>`;
-  editButton.innerHTML = `<div class="edit-buttons-${entryCounter}"><i onclick="editComplete('${entryCounter}')" class="fa-solid fa-square-check"></i><button onclick="removeEntry('${entryCounter}')">X</button></div>`;
-  const editTitleValue = document.getElementById(`title-input=${entryCounter}`).innerText;
-  editTitleValue = titleInitialValue;
-  const editDescriptionValue = document.getElementById(
-    `description-input=${entryCounter}`
-  ).innerText;
-  editDescriptionValue = descriptionInitialValue;
+  const title = document.getElementById(`title-${entryCounter}`);
+  const description = document.getElementById(`description-${entryCounter}`);
+  title.innerHTML = `<input placeholder="Title" id="title-input-${entryCounter}" type="text" value="${titleInitialValue}"><br>`;
+  description.innerHTML = `<textarea id="description-input-${entryCounter}" placeholder="Description" cols="40" rows="10">${descriptionInitialValue}</textarea>`;
+  buttonContainer.innerHTML = `<i onclick="editComplete(${entryCounter})" class="fa-solid fa-square-check check-icon"></i>
+  <i onclick="removeEntry(${entryCounter})" class="fa-solid fa-trash remove-icon"></i>
+    `;
 }
 
-//function reverts complete edits
+// function reverts complete edits
 function editComplete(entryCounter) {
   const titleEditInput = document.getElementById(`title-input-${entryCounter}`);
   const descriptionEditInput = document.getElementById(
     `description-input-${entryCounter}`
   );
   const entry = document.getElementById(`entry-${entryCounter}`);
-  entry.innerHTML = `<div id="entry-${entryCounter}"><h3 id="title-${entryCounter}">${titleEditInput.value}</h3><p id="description-${entryCounter}">${descriptionEditInput.value}</p><i onclick="editEntry('${entryCounter}')" id="edit-icon-${entryCounter}" class="fa-solid fa-pen-to-square"></i><br></div>`;
+
+  //initial values
+  const titleEditedValue=titleEditInput.value;
+  const descriptionEditedValue=descriptionEditInput.value;
+
+  entry.innerHTML = `
+      <div id="entry-${entryCounter}">
+        <h3 id="title-${entryCounter}">${titleEditedValue}</h3>
+        <p id="description-${entryCounter}">${descriptionEditedValue}</p>
+        <div id="button-container-${entryCounter}"><i onclick="editEntry(${entryCounter})" class="fa-solid fa-pen-to-square edit-icon"></i></div>
+        <br>
+      </div>`;
 }
 
-//remove entry function
-
+// remove entry function
 function removeEntry(entryCounter) {
   const entry = document.getElementById(`entry-${entryCounter}`);
   entry.remove();
